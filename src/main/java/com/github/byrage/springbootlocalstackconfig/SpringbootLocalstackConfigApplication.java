434a928c4984935dc5dbbe85ea6394fa756bfcd8
byrage <byrage91@gmail.com>
2020-03-06 08:18:02 +0900

    Init

---
+++ src/main/java/com/github/byrage/springbootlocalstackconfig/SpringbootLocalstackConfigApplication.java
@@ -0,0 +1,13 @@
+package com.github.byrage.springbootlocalstackconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLocalstackConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLocalstackConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/byrage/springbootlocalstackconfig/SpringbootLocalstackConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.byrage.springbootlocalstackconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLocalstackConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
